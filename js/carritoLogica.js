41c75730309470a348a87d526987944fefd5d97d
Moises <moche10911@gmail.com>
2022-11-06 17:12:59 -0500

    Anadido la fucncion de seguir usando el carrito de la ultima sesion del usuario

--- js/carritoLogica.js
+++ js/carritoLogica.js
@@ -1,5 +1,19 @@
 let carrito = []
 let contenedor = document.getElementById("misprods")
+
+function agregarAlCarrito(galletaComp){
+    carrito.push(galletaComp)
+    document.getElementById("tablaboddy").innerHTML +=`
+        <tr>
+            <td>${galletaComp.id}</td>
+            <td>${galletaComp.nombre}</td>
+            <td>${galletaComp.precio}</td>
+        </tr>
+    `;
+    let totalCarrito = carrito.reduce((acummulador, producto)=>acummulador + producto.precio, 0)
+    document.getElementById("total").innerText="Total a pagar: $"+totalCarrito
+    localStorage.setItem("carritoUsuario",JSON.stringify(carrito))
+}
 //Esta funcion renderiza y anade funcion a los eventos de los botones de las cartas para los productos
 function renderizarproductos() {
     for (const galleta of galletas) {
@@ -18,21 +32,31 @@ function renderizarproductos() {
     for(let galleta of galletas){
         document.getElementById(`btn${galleta.id}`).addEventListener("click", function (){
             agregarAlCarrito(galleta)
-            localStorage.setItem("carritoUsuario",JSON.stringify(carrito))
         })
     }
 }
-function agregarAlCarrito(galletaComp){
-    carrito.push(galletaComp)
-    console.table(carrito)
-    document.getElementById("tablaboddy").innerHTML +=`
-        <tr>
-            <td>${galletaComp.id}</td>
-            <td>${galletaComp.nombre}</td>
-            <td>${galletaComp.precio}</td>
-        </tr>
-    `;
-    let totalCarrito = carrito.reduce((acummulador, producto)=>acummulador + producto.precio, 0)
-    document.getElementById("total").innerText="Total a pagar: $"+totalCarrito
+let tabla = document.getElementById("tablabody")
+if(localStorage.getItem("carritoUsuario")!= null){
+    let respUsuario = prompt("Desesa seguir con su compra?(si o no)").toLowerCase()
+    if(respUsuario == "si"){
+        carrito = JSON.parse(localStorage.getItem("carritoUsuario"))
+        console.log(carrito)
+        for(let productos of carrito){
+            document.getElementById("tablaboddy").innerHTML +=`
+            <tr>
+                <td>${productos.id}</td>
+                <td>${productos.nombre}</td>
+                <td>${productos.precio}</td>
+            </tr>
+        `;
+        }
+        
+        renderizarproductos()
+    }if(respUsuario == "no"){
+        carrito = []
+        localStorage.clear()
+        renderizarproductos()
+    }
+}else{
+    renderizarproductos()
 }
-renderizarproductos()
